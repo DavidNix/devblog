f895d15fd6327e91a07e2d270d744946df6d722e
David Nix <david@sheldonsowell.com>
2012-07-17 14:07:24 -0600

    Initial commit. Add devise default installation including admin model and views.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Devblog::Application.config.secret_token = 'aba1f9b850b19e3cb83c917f61484b48028246de3a5183289d44028441b154a75e2bc58c3021eb5012133a13ce3b9ee4c6f6cb2a0913eb2128532a52bda4daa4'
