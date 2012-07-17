572893f63c61de481b91f55b6fc055a5aff5dc5c
David Nix <david@sheldonsowell.com>
2012-07-17 14:51:22 -0600

    Add static pages and respective rspecs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+# Returns the full title on a per-page basis.
+# Set the base title here
+  def full_title(page_title)
+    base_title = "DevBlog"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
+
 end
