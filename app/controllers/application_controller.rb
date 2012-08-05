91a591760e24b9a914a3b9c7c47a7524fc80d35d
David Nix <david@sheldonsowell.com>
2012-08-05 14:43:25 -0600

    Fix the posts index template.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  # overriding devise methods
+  # overriding Devise path
   def after_sign_in_path_for(resource)
   	posts_path
   end
--- lib/devblog_extensions.rb
+++ lib/devblog_extensions.rb
@@ -8,9 +8,8 @@ class Time
 			year = Time.now.year - rand(years_back)
 			month = rand(12) + 1
 			# account for 28 days in Feb, not caring about leap years
-			day = month == 2 ? rand(28) + 1 : rand(31) + 1
+			day = month == 2 ? rand(28) + 1 : rand(30) + 1
 			Time.local(year, month, day)
-		
 	end
 end
 
