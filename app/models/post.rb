dc04e3329fe920600a3f0f1c4b9619b99c04bab0
David Nix <david@sheldonsowell.com>
2012-08-14 13:31:55 -0600

    Add parameterize method to Post model.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,16 +1,14 @@
 class Post < ActiveRecord::Base
   attr_accessible :content, :permalink, :release_date, :teaser, :title
 
-  before_save { |post| post.permalink = permalink.downcase.strip.gsub(/ +/, "-") }
+  before_save { |post| post.permalink = permalink.parameterize }
 
   validates :title, presence: true
   validates :teaser, presence: true
   validates :content, presence: true
   validates :release_date, presence: true
 
-  # TO DO:  make a good regex for permalinks
-  # VALID_PERMALINK_REGEX = /[a-z0-9]+/i
-  validates :permalink, presence: true, uniqueness: { case_sensitive: false } #format: { with: VALID_PERMALINK_REGEX }
+  validates :permalink, presence: true, uniqueness: { case_sensitive: false }
 
 
   def published_date 
