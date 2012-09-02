4f8c939adb0416760c4f1d5fd47d3c4ab04fbc40
David Nix <david@sheldonsowell.com>
2012-09-02 14:57:28 -0600

    Add atom auto discover link to layout and specs.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,6 +8,7 @@ describe "StaticPages" do
     before { visit root_path }
     it { should have_selector('title', text: full_title('')) }
     it { should_not have_selector 'title', text: '| Home' }
+    it { should have_xpath("//html/head/link[@href='#{articles_url}.atom']")}
   end
 
   describe "About page" do
