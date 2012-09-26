8e685b909c2e9d31f368d1db941e6899cc86d3e0
David Nix <david@sheldonsowell.com>
2012-09-26 09:44:34 -0600

    Add logic and fix directory structure to allow for different layouts.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,38 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  # overriding Devise path
+  layout :app_or_admin
+
+  # overriding Devise
   def after_sign_in_path_for(resource)
   	posts_url
+  end
+
+  private
+  # returns the appropriate layout
+  def app_or_admin
+  	puts params[:controller].to_s.red
+  	if is_blog_admin_controller?
+  		"blog_admin/admin"
+  	else
+  		"application"
+  	end
+
+  end
+
+  # in the future, logic may need to be changed if you want to use Devise for other purposes, like users
+  def is_blog_admin_controller?
+  	# add specific controllers here
+  	blog_admin_controllers = %w{posts admin_registrations}
+  	blog_admin_controllers.each do |controller|
+  		param_controller = params[:controller]
+  		if param_controller == controller || param_controller =~ /devise/i
+  			return true
+  		end
+  	end
+
+  	return false
+
   end
 
 end
