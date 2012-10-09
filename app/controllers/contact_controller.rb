88cfaf0491a5050c86b051f4a751758c709912f8
David Nix <davidnix81@gmail.com>
2012-10-08 22:56:04 -0600

    Add email only partial. Trying to modify controller to render the previous action and controller.  Still needs work.

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -7,11 +7,21 @@ class ContactController < ApplicationController
 	def create
 		@message = Message.new(params[:message])
 
-		if @message.valid?
-			ContactMailer.new_message(@message).deliver
-			redirect_to(contact_path, :notice => "Your message was successfully sent.")
+		# email_only partial in contact view captures the previous action and controller
+		if params[:email_only] == "true"
+			if @message.valid?
+				ContactMailer.new_message(@message).deliver
+				redirect_to(:back, :notice => "Your message was successfully sent.")
+			else
+				render params[:prev_controller] + "/" + params[:prev_action]
+			end
 		else
-			render :new
+			if @message.valid?
+				ContactMailer.new_message(@message).deliver
+				redirect_to(contact_path, :notice => "Your message was successfully sent.")
+			else
+				render :new
+			end
 		end
 	end
 
