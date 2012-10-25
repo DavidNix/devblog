fe473048589766d814915fc28cd93ca8b5908ec9
David Nix <david@sheldonsowell.com>
2012-10-25 16:15:34 -0600

    Add one more spec to the social icons helper spec.

--- spec/helpers/social_icons_helper_spec.rb
+++ spec/helpers/social_icons_helper_spec.rb
@@ -6,6 +6,10 @@ describe SocialIconsHelper do
     helper.sm_icon(name: "twitter", tool_title: "Test title", href: sm_twitter_path).should eq("<a class=\"sm_32_icon sm_32_twitter sm_shadow\" data-placement=\"bottom\" href=\"/social_icons/twitter\" rel=\"tooltip\" title=\"Test title\"><span></span></a>")
   end
 
+  it "should produce a valid 32 pixel icon without a tooltip" do
+    helper.sm_icon(name: "twitter", href: sm_twitter_path).should eq("<a class=\"sm_32_icon sm_32_twitter sm_shadow\" data-placement=\"bottom\" href=\"/social_icons/twitter\" rel=\"\"><span></span></a>")
+  end
+
   it "should produce a valid 64 pixel icon without a shadow" do
     helper.sm_icon(name: "rss", tool_title: "Test title", href: sm_rss_path, shadow: false, size: 64).should eq("<a class=\"sm_64_icon sm_64_rss \" data-placement=\"bottom\" href=\"/social_icons/rss\" rel=\"tooltip\" title=\"Test title\"><span></span></a>")
   end
