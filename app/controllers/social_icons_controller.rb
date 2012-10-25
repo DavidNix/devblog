ed2e8499ef63811f9b6c88633fba341176112f94
David Nix <david@sheldonsowell.com>
2012-10-25 14:18:09 -0600

    Implement controller and controller specs.

--- app/controllers/social_icons_controller.rb
+++ app/controllers/social_icons_controller.rb
@@ -1,10 +1,16 @@
 class SocialIconsController < ApplicationController
+
+	# Purpose of this controller is to allow testing different subscribe options using a gem such as Split.
+
   def rss
+  	redirect_to DevblogExtensions::FEED_URL
   end
 
   def email
+  	redirect_to DevblogExtensions::EMAIL_FEED_URL
   end
 
   def twitter
+  	redirect_to DevblogExtensions::TWITTER_URL
   end
 end
